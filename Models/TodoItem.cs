7cee9643e6231bd56b21576b612d0f00cb3261f3
Scott Kuhl <scott@kuhl.ws>
2024-04-07 11:19:05 -0500

    Default Due
    - Increased default due date to next day.

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -6,7 +6,7 @@ public class TodoItem
 {
     public bool Completed { get; set; }
 
-    public DateTime Due { get; set; } = DateTime.Today;
+    public DateTime Due { get; set; } = DateTime.Today.AddDays(1);
 
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public int Id { get; set; }
